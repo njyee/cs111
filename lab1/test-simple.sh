6312ed1d4344672c8268e2b57bc6abc71a276a6a
njyee <nickjyee@gmail.com>
2014-10-14 09:33:57 +0000

    test script 1

---
+++ lab1/test-simple.sh
@@ -0,0 +1,45 @@
+#! /bin/sh
+
+# UCLA CS 111 Lab 1 - Test that valid syntax is processed correctly.
+
+# Copyright 2012-2014 Paul Eggert.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+tmp=$0-$$.tmp
+mkdir "$tmp" || exit
+
+(
+cd "$tmp" || exit
+
+cat >test.sh <<'EOF'
+true
+EOF
+
+cat >test.exp <<'EOF'
+# 1
+  true
+EOF
+
+../profsh -t test.sh >test.out 2>test.err || exit
+
+diff -u test.exp test.out || exit
+test ! -s test.err || {
+  cat test.err
+  exit 1
+}
+
+) || exit
+
+rm -fr "$tmp"
