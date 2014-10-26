f27cfc3aa4556955a1f234a276802e93bf4b29bd
nickjyee <nicholas@s-169-232-83-94.resnet.ucla.edu>
2014-10-25 19:08:17 -0700

    test script script.sh added

---
+++ lab1/script.sh
@@ -0,0 +1,68 @@
+# simple command
+
+ls
+
+echo foo
+
+
+
+# simple command exec
+
+exec echo bar
+
+
+
+# simple command I/O redirection
+
+ls -al > a.txt
+
+cat < a.txt
+
+cat < a.txt > b.txt
+
+
+
+# subshell command
+
+(echo hello world)
+
+
+
+# subshell I/O redirection
+
+(cat) <b.txt
+
+(cat <b.txt) >c.txt
+
+(cat) <c.txt >d.txt
+
+
+
+# sequence command
+
+echo a; echo b
+
+echo c
+echo d
+
+
+
+# pipe command
+
+cat < d.txt | echo
+
+cat d.txt | tr a-z A-Z | sort -u
+
+ls -l |
+cat
+
+
+
+# if command
+
+if echo 1
+then echo 2
+fi
+
+# while command
+# until command
