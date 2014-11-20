ff666397965c8037ef7324b2b1dd45e77f2a00f3
Nicholas Yee <njyee@ucla.edu>
2014-11-20 01:54:46 -0800

    ospfs_write() implemented

--- lab3/ospfsmod.c
+++ lab3/ospfsmod.c
@@ -1006,11 +1006,14 @@ ospfs_read(struct file *filp, char __user *buffer, size_t count, loff_t *f_pos)
 		/* EXERCISE: Your code here */
 		//retval = -EIO; // Replace these lines
 		//goto done;
+
 		offset = *f_pos % OSPFS_BLKSIZE;
+
 		if (count - amount < OSPFS_BLKSIZE - offset)
 			n = count - amount;
 		else
 			n = OSPFS_BLKSIZE - offset;
+
 		if (copy_to_user(buffer, data + offset, n)) {
 			retval = -EFAULT;
 			break;
@@ -1053,20 +1056,25 @@ ospfs_write(struct file *filp, const char __user *buffer, size_t count, loff_t *
 	// Support files opened with the O_APPEND flag.  To detect O_APPEND,
 	// use struct file's f_flags field and the O_APPEND bit.
 	/* EXERCISE: Your code here */
+	if (filp->f_flags & O_APPEND)
+		*f_pos = oi->oi_size;
 
 	// If the user is writing past the end of the file, change the file's
 	// size to accomodate the request.  (Use change_size().)
 	/* EXERCISE: Your code here */
+	if (oi->oi_size < *f_pos + count)
+		change_size(oi, *f_pos + count);
 
 	// Copy data block by block
 	while (amount < count && retval >= 0) {
 		uint32_t blockno = ospfs_inode_blockno(oi, *f_pos);
-		uint32_t n;
+		uint32_t n, offset;
 		char *data;
 
 		if (blockno == 0) {
 			retval = -EIO;
-			goto done;
+			//goto done;
+			break;
 		}
 
 		data = ospfs_block(blockno);
@@ -1076,15 +1084,27 @@ ospfs_write(struct file *filp, const char __user *buffer, size_t count, loff_t *
 		// read user space.
 		// Keep track of the number of bytes moved in 'n'.
 		/* EXERCISE: Your code here */
-		retval = -EIO; // Replace these lines
-		goto done;
+		//retval = -EIO; // Replace these lines
+		//goto done;
+
+		offset = *f_pos % OSPFS_BLKSIZE;
+
+		if (count - amount < OSPFS_BLKSIZE - offset)
+			n = count - amount;
+		else
+			n = OSPFS_BLKSIZE - offset;
+
+		if (copy_from_user(data + offset, buffer, n)) {
+			retval = -EFAULT;
+			break;
+		}
 
 		buffer += n;
 		amount += n;
 		*f_pos += n;
 	}
 
-    done:
+    //done:
 	return (retval >= 0 ? amount : retval);
 }
 
